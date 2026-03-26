e33218d95ac35f1a32f3f9e63b53df27a1461520
Ubuntu <ubuntu@ip-172-31-9-210.us-east-2.compute.internal>
2026-03-26 19:00:01 +0000

    fix: ecr

--- infrastructure/pull.sh
+++ infrastructure/pull.sh
@@ -1,2 +1,2 @@
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker pull public.ecr.aws/neuron/pytorch-inference-vllm-neuronx:0.11.0-neuronx-py312-sdk2.27.0-ubuntu24.04
+docker pull public.ecr.aws/neuron/pytorch-inference-vllm-neuronx:0.13.0-neuronx-py312-sdk2.28.0-ubuntu24.04
